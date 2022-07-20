1a62cfabfe96b501f2a11f7bcef7ed94e34eb9dd
The-Dumpy <109386200+The-Dumpy@users.noreply.github.com>
2022-07-20 09:55:22 -0700

    Add project files.

---
+++ Chapter 3 Factors/Chapter 3 Factors.cpp
@@ -0,0 +1,20 @@
+
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int value = 0;
+    cout << "Please enter a positive integer\n";
+    cin >> value;
+    cout << "The factors of " << value << ": 1";
+    for (int i = 2; i <= value/2; i++)
+    {
+        if (value % i == 0)
+        {
+            cout << ", " << i;
+
+        }
+    }
+}
